a1af49f406a6b14e98b2045a489af9b6a5df2b0c
Daniel Chukwu <00danzy@gmail.com>
2022-08-02 13:53:43 +0100

    fix main method indentation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
-        public static void main (String [] args) {
-            Exeat app1 = new Exeat(); // create object
-            app1.ExeatApp(); // call method with object
-        }
+    public static void main (String [] args) {
+        Exeat app1 = new Exeat(); // create object
+        app1.ExeatApp(); // call method with object
+    }
 }
